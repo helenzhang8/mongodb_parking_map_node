84c7d3d12104ae45ca3dbe8bb5471b42d056d33b
helenzhang8 <suki22889@gmail.com>
2021-06-23 22:46:06 -0400

    removed cors

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const path = require('path');
-const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
-app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://areaprobe:parking@cluster0.jzp7s.mongodb.net/parking?retryWrites=true&w=majority";
