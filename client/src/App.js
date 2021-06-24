2ce25824034e54ce09e63c86fb4aba49a8fd4c22
Helen Zhang <66694809+helenzhang8@users.noreply.github.com>
2021-06-23 20:35:13 -0400

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./App.css";
+import Map from './MapViewScratch.js';
+
+function App() {
+  const [data, setData] = React.useState([]);
+
+  React.useEffect(() => {
+      setTimeout(()=>{
+          fetch("/api")
+          .then((res1) => res1.json())
+          .then((res2) => {
+              setData(res2.message)});},100)
+  }, []);
+
+  return (
+      <div className="App">
+        {/*<div>*/}
+        {/*    {data.map((d) => <li key={d._id}>{d._id},{d.latlon},{d.status.toString()}</li>)}*/}
+        {/*</div>*/}
+          <Map allLocations={data} setMarkerToDisplay={null}/>
+      </div>
+  );
+}
+
+export default App;
