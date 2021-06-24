03cf3617d205c0bb589585fbb4f6e15dd5d39c42
helenzhang8 <suki22889@gmail.com>
2021-06-23 22:38:19 -0400

    removed client dependent line

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+app.use(cors());
 
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://areaprobe:parking@cluster0.jzp7s.mongodb.net/parking?retryWrites=true&w=majority";
@@ -17,8 +19,6 @@ client.connect(err => {
     // perform actions on the collection object
     // client.close();
 });
-
-app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: list });
